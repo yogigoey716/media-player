aad79c3b00b2eb0eb3193a9ae70387a99b101d30
Yogi716 <prayoginugraha@gmail.com>
2025-05-10 15:03:03 +0700

    Edit themes and added strings value

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,12 +46,16 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.fragment.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation(libs.retrofit)
-    implementation(libs.logging.interceptor)
+    implementation (libs.okhttp)
     implementation(libs.converter.gson)
 
     implementation(libs.koin.android)
@@ -59,4 +63,18 @@ dependencies {
 
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
+
+
+    implementation(libs.coil)
+    implementation(libs.coil.gif)
+
+
+    // Kotlin
+    implementation (libs.androidx.navigation.fragment.ktx)
+    implementation (libs.androidx.navigation.ui.ktx)
+
+
+    implementation (libs.androidx.media3.exoplayer)
+
+
 }
